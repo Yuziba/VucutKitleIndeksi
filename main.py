f5c5193ce0aaa19c014c81c163a42c779400949a
Yusuf Ziya BAYRAM <yusufzbayram@outlook.com>
2023-09-02 19:48:43 +0300

    Cokme sorunu giderildi

--- main.py
+++ main.py
@@ -6,22 +6,25 @@ window.minsize(width=300, height=500)
 
 sonuc = ""
 def KiloBilgisiAl():
-    kullaniciKilosu = int(entryKilo.get())      #Kilogram
-    kullaniciBoyu = int(entryBoy.get())/100     #Metre
-    vki = kullaniciKilosu/(kullaniciBoyu * kullaniciBoyu)
+    try:
+        kullaniciKilosu = int(entryKilo.get())  # Kilogram
+        kullaniciBoyu = int(entryBoy.get()) / 100  # Metre
+        vki = kullaniciKilosu / (kullaniciBoyu * kullaniciBoyu)
+        if vki < 18:
+            sonuc = "Cok Zayifsiniz"
+        elif 18 <= vki < 25:
+            sonuc = "Kilonuz ideal"
+        elif 25 <= vki < 30:
+            sonuc = "Kilonuz biraz fazla"
+        elif vki >= 30:
+            sonuc = "Cooook kilolusnuz"
+        else:
+            sonuc = "gecerli bir deger giriniz"
+        print(vki)
+        labelSonuc.config(text=sonuc)
+    except:
+        labelSonuc.config(text="hatali deger")
 
-    if vki<18:
-        sonuc = "Cok Zayifsiniz"
-    elif 18<=vki<25:
-        sonuc = "Kilonuz ideal"
-    elif 25<=vki<30:
-        sonuc = "Kilonuz biraz fazla"
-    elif vki>=30:
-        sonuc = "Cooook kilolusnuz"
-    else:
-        sonuc = "gecerli bir deger giriniz"
-    print(vki)
-    labelSonuc.config(text=sonuc)
 
 
 labelKilo = tkinter.Label(text="Kilo bilginizi giriniz")
@@ -38,18 +41,8 @@ entryBoy.pack()
 buttonKilo = tkinter.Button(text="Onayla",command=KiloBilgisiAl)
 buttonKilo.pack(pady=20)
 
-labelSonuc = tkinter.Label(text="aa")
+labelSonuc = tkinter.Label(text="")
 labelSonuc.pack()
-
-
-
-
-
-
-
-
-
-
 
 
 window.mainloop()
